09a35e624d90f7ddba2092d7c9c8d6f1028402c6
Kieran Griffiths <20491633@student.uwa.edu.au>
2021-11-23 14:29:29 +0800

    Started on a configuration scheme

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,57 @@ use std::net::SocketAddr;
 
 use axum::Router;
 
+trait Config {
+    fn port_message_action(&self, port: u16);
+}
+
+struct DebugConfig;
+
+impl Config for DebugConfig {
+    fn port_message_action(&self, port: u16) {
+        println!("serve on http://localhost:{}", port)
+    }
+}
+
+struct HerokuConfig;
+
+impl Config for HerokuConfig {
+    fn port_message_action(&self, port: u16) {
+        println!("serve on https://antecedence.herokuapp.com:{}", port)
+    }
+}
+
 #[tokio::main]
-async fn main(){
+async fn main() {
     make_and_run_app().await;
 }
 
 async fn make_and_run_app() {
+    let config = get_config();
     let app = Router::new().route("/", axum::routing::get(index));
-    let addr = get_address();
+    let addr = get_address(config.as_ref());
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-fn get_address() -> SocketAddr {
+fn get_config() -> Box<dyn Config> {
+    if let Ok(value) = std::env::var("CONFIG") {
+        if value == "heroku" {
+            return Box::new(HerokuConfig);
+        }
+    }
+    Box::new(DebugConfig)
+}
+
+fn get_address(config: &dyn Config) -> SocketAddr {
     let port = std::env::var("PORT")
         .unwrap_or_else(|_| "8080".to_owned())
         .parse()
         .expect("PORT must be a number");
-    let addr = SocketAddr::from(([0,0,0,0], port));
-    println!("serve from http://localhost:{}", port);
-    addr
+    config.port_message_action(port);
+    ([0, 0, 0, 0], port).into()
 }
 
 /// Test with just a static "Hello, World!" string
